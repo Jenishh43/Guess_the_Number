d0b3a4878161f9b7d2819b03128443fc3f3aaa49
Rajguru Jenish <RAJGURUJENISH343@GMAIL.COM>
2024-11-03 10:14:06 +0000

    basic

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import random
+x = int(input("Enter a Minmum Number: "))
+y = int(input("Enter a Maximum Number: "))
 
+if (type(x and y) == int):
+    print('good')
+else:
+    print("galti na karo")
+
+randomNumber = random.randint(x, y)
+print(randomNumber)
