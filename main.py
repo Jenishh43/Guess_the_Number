4433c0923b1bb6975354ddd855d91c916cc0d5e0
Rajguru Jenish <RAJGURUJENISH343@GMAIL.COM>
2024-11-03 14:36:05 +0000

    Basic Guess Number Game

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import random
-x = int(input("Enter a Minmum Number: "))
-y = int(input("Enter a Maximum Number: "))
 
-if (type(x and y) == int):
-    print('good')
-else:
-    print("galti na karo")
+MinNum = 0
+MaxNum = 100
+attempt = 10
+PreviousGuesses = []
+randomNumber = random.randint(MinNum, MaxNum)
+# print(randomNumber)
 
-randomNumber = random.randint(x, y)
-print(randomNumber)
+while attempt != 0:
+    try:
+        EnteredNum = int(input("Enter a number: "))
+    except ValueError:
+        print("Invalid input. Please enter an integer.\n")
+        continue
+
+    PreviousGuesses.append(EnteredNum)
+    attempt -= 1
+    
+    if(randomNumber < EnteredNum):
+        print(f"Try to Guess Low , Remaining:- {attempt}, Your Previous Guess:- {PreviousGuesses}\n")
+    elif(randomNumber > EnteredNum):
+        print(f"Try to Guess High, Remaining:- {attempt}, Your Previous Guess:- {PreviousGuesses}\n")
+    elif(randomNumber == EnteredNum):
+        print(f"You Win, Remaining:- {attempt}, Your Previous Guess:- {PreviousGuesses}\n")
+        break
+else:        
+    print(f"You Lost, Your Previous Guess:- {PreviousGuesses}\n")
